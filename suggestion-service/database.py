127b55667cbb9aa47b224c1754374502bc4b4164
Kevin <kevisos123@gmail.com>
2024-04-07 18:11:01 -0500

    Connected DB?
    //
    Added table

--- suggestion-service/database.py
+++ suggestion-service/database.py
@@ -3,15 +3,14 @@ import psycopg2
 conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="admin", port=5432)
 
 cur = conn.cursor()
-cur.execute("""
-CREATE TABLE IF NOT EXISTS suggestions (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) not null,
-    description TEXT,
-    category VARCHAR(255)
-    );
-""")
 
+cur.execute("""CREATE TABLE IF NOT EXISTS suggestions (
+    id SERIAL PRIMARY KEY,
+    category VARCHAR(255),
+    title VARCHAR(255),
+    desc VARCHAR(255)
+)"""
+)
 
 
 
