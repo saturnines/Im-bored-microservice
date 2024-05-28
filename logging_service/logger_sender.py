ef821ca6c66d8a3c0a2a078e6269a24b3c7ac7a3
Kevin <kevisos123@gmail.com>
2024-05-28 16:47:27 -0500

    Added fluent.conf, added docker to use with kibana and eleastic, added fluent function for logging
    Added elastic, kibana
    
    // TODO
    add fluentd logging
    frontend
    Add testing microservice

---
+++ logging_service/logger_sender.py
@@ -0,0 +1,16 @@
+import logging
+from fluent import handler as fluent_handler
+
+def configure_logging(service_name: str, fluentd_host: str = 'localhost', fluentd_port: int = 24224):
+    # Create or get current logger
+    logger = logging.getLogger(service_name)
+    logger.setLevel(logging.INFO)
+
+    # check if theres a logger
+    if not logger.handlers:
+        fluent_handler_instance = fluent_handler.FluentHandler(service_name, host=fluentd_host, port=fluentd_port)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fluent_handler_instance.setFormatter(formatter)
+        logger.addHandler(fluent_handler_instance)
+
+    return logger
