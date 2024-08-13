c1789e248451a4762005d78e26955f136226bc45
Kevin <kevisos123@gmail.com>
2024-08-13 04:41:17 -0500

    added to docker context file.

---
+++ docker_context_files/logging_service/logger_sender.py
@@ -0,0 +1,28 @@
+import logging
+from fluent import sender
+
+print("Hello from logging Service")
+
+
+def configure_logging(service_name: str, fluentd_host: str = 'localhost', fluentd_port: int = 24224):
+    # Create or get current logger
+    logger = logging.getLogger(service_name)
+    logger.setLevel(logging.INFO)
+
+    # check if there's a logger if not just set default logger with below configs
+    if not logger.handlers:
+        fluent_sender = sender.FluentSender(service_name, host=fluentd_host, port=fluentd_port)
+
+        class FluentHandler(logging.Handler):
+            def emit(self, record):
+                fluent_sender.emit(record.name, {
+                    'level': record.levelname,
+                    'message': self.format(record),
+                })
+
+        fluent_handler_instance = FluentHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fluent_handler_instance.setFormatter(formatter)
+        logger.addHandler(fluent_handler_instance)
+
+    return logger
